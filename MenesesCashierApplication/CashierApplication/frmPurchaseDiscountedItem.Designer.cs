9660bb9b20aa1928f24515a7485fba008e3fd903
Jroo <159931577+beetsoul@users.noreply.github.com>
2026-05-23 09:20:58 +0800

    Add files via upload

---
+++ MenesesCashierApplication/CashierApplication/frmPurchaseDiscountedItem.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CashierApplication
+{
+    partial class frmPurchaseDiscountedItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.discountTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.computeBtn = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.receivedTextBox = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Location = new System.Drawing.Point(15, 51);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(198, 20);
+            this.itemTextBox.TabIndex = 1;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(59, 77);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(154, 20);
+            this.priceTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Price:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(216, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Discount (%):";
+            // 
+            // discountTextBox
+            // 
+            this.discountTextBox.Location = new System.Drawing.Point(219, 51);
+            this.discountTextBox.Name = "discountTextBox";
+            this.discountTextBox.Size = new System.Drawing.Size(129, 20);
+            this.discountTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(216, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantity:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(282, 77);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(67, 20);
+            this.quantityTextBox.TabIndex = 7;
+            // 
+            // computeBtn
+            // 
+            this.computeBtn.Location = new System.Drawing.Point(138, 117);
+            this.computeBtn.Name = "computeBtn";
+            this.computeBtn.Size = new System.Drawing.Size(75, 23);
+            this.computeBtn.TabIndex = 8;
+            this.computeBtn.Text = "Compute";
+            this.computeBtn.UseVisualStyleBackColor = true;
+            this.computeBtn.Click += new System.EventHandler(this.computeBtn_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.totalLabel.Location = new System.Drawing.Point(83, 156);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(202, 27);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.Text = "Total amount: ";
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.totalLabel.Click += new System.EventHandler(this.totalLabel_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(12, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Payment Received:";
+            // 
+            // receivedTextBox
+            // 
+            this.receivedTextBox.Location = new System.Drawing.Point(148, 214);
+            this.receivedTextBox.Name = "receivedTextBox";
+            this.receivedTextBox.Size = new System.Drawing.Size(121, 20);
+            this.receivedTextBox.TabIndex = 11;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(275, 211);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 12;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.changeLabel.Location = new System.Drawing.Point(83, 254);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(202, 27);
+            this.changeLabel.TabIndex = 13;
+            this.changeLabel.Text = "Change: ";
+            this.changeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmPurchaseDiscountedItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(361, 295);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.receivedTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.computeBtn);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.discountTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPurchaseDiscountedItem";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPurchaseDiscountedItem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox discountTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Button computeBtn;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox receivedTextBox;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label changeLabel;
+    }
+}
+
